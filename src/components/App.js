8f52ca49c9da133c1b9c7b5008038d73365a9e34
Anna Galkina <sandhani1704@yandex.ru>
2021-01-29 00:34:48 +0300

    feat: add

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,8 @@
 import './App.css';
 import React from 'react';
+import useInterval from "@use-it/interval";
+import man from '../images/man.svg'
+import woman from '../images/woman.svg'
 
 function App() {
   const [messageToShow, setMessageToShow] = React.useState(0)
@@ -15,11 +18,19 @@ function App() {
     { text: '20ReactApps.com' }
 
   ]
+
+  useInterval(() => {
+    setMessageToShow((messageToShow) => messageToShow + 1)
+  }, 2000)
+
   return (
     <div className="app">
       <div className="walkthrough">
         {messages.map((message, index) => {
+          const even = index % 2 === 0;
+          if (messageToShow + 1 === index) return <Typing key={index} even={even} />
 
+          if (index > messageToShow) return <div key={index} />
 
 
           return <Message key={index} message={message} />
@@ -33,10 +44,25 @@ function App() {
 function Message({ message }) {
   return (
     <div className="message">
-      <div className="avatar"></div>
+      <div className="avatar"><img className='avatar__image' src={man} alt={man} /></div>
       <div className="text">{message.text}</div>
-      <div className="avatar"></div>
+      <div className="avatar"><img className='avatar__image' src={woman} alt={woman} /></div>
     </div>
   )
 }
+
+function Typing() {
+  return (
+    <div className="typing is-right is-left">
+      <div className="dots">
+        <div />
+        <div />
+        <div />
+
+      </div>
+
+    </div>
+  )
+}
+
 export default App;
